1d015c97c52eb90a9a99c5a0047e6a9816ebbf0a
Bablu jani <160379580+Manzoorahmed76@users.noreply.github.com>
2024-03-03 15:07:44 +0500

    Add files via upload

---
+++ js/g9zcs0mzap9q7lb5.js
@@ -0,0 +1,9 @@
+<html><head><title>403 Forbidden</title></head>
+ <body>
+  <h1>403 Forbidden</h1>
+  <ul>
+   <li>Code: AccessDenied</li>
+   <li>BucketName: panels-cdn</li>
+   <li>RequestId: tx000002aa9e8fc4756fbe6-0065e431da-3fba010-prg</li>
+   <li>HostId: 3fba010-prg-eu-1</li>
+</ul></body></html>
---
+++ js/pnl24maick8i2jzj.js
@@ -0,0 +1,9 @@
+<html><head><title>403 Forbidden</title></head>
+ <body>
+  <h1>403 Forbidden</h1>
+  <ul>
+   <li>Code: AccessDenied</li>
+   <li>BucketName: panels-cdn</li>
+   <li>RequestId: tx00000ab7bd68bf098658e-0065e431da-48c66ec-prg</li>
+   <li>HostId: 48c66ec-prg-eu-1</li>
+</ul></body></html>
